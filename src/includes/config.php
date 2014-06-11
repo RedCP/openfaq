1112244180a8f57e12d3a33838c4fd9188e5f9cd
Thomas Anderson <redcp2009@gmail.com>
2014-06-11 03:54:46 -0400

    Config file
    Configure your stuff!

---
+++ src/includes/config.php
@@ -0,0 +1,10 @@
+<?php
+	$host = "localhost"; // Change to your host
+	$user = "root"; // Change to your user
+	$pass = ""; // Enter your password
+	$db = "openfaq"; // Do not change unless you modify database name
+	
+	$title = "OpenFAQ"; // You can change the title of your tab here
+	$keywords = "openfaq, th0m.as, thomas, redcp, sugarfruit studios, sugarfruit"> // Meta keywords
+	$description = "OpenFAQ is a simple question-to-answer system written with PHP."> // Meta description
+?>
