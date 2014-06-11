f9940b710909a1c8f90c4f2527f6af1b64aecb3b
Thomas Anderson <redcp2009@gmail.com>
2014-06-11 03:08:15 -0400

    New stuff!
    Config and connect.

---
+++ src/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+	$host = "localhost"; // Change to your host
+	$user = "root"; // Change to your user
+	$pass = ""; // Enter your password
+	$db = "openfaq"; // Do not change unless you modify database name
+	$title = "OpenFAQ"; // You can change the title of your tab here
+?>
---
+++ src/includes/connect.php
@@ -0,0 +1,4 @@
+<?php
+	include('/src/includes/config.php');
+	$con = mysqli_connect('$host', '$user', '$pass', '$db') or die(mysqli_error());
+?>
